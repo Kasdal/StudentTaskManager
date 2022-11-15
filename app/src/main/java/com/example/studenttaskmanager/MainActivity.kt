ba0d511f61714f8bfc39f98e453952581680854e
Milan Ples <plesmilan@hotmail.com>
2022-11-15 13:13:51 +0000

    Added corner menu in the app this will serve future sorting options or the tasks.

--- app/src/main/java/com/example/studenttaskmanager/MainActivity.kt
+++ app/src/main/java/com/example/studenttaskmanager/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.studenttaskmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.studenttaskmanager.databinding.ActivityMainBinding
@@ -13,7 +16,10 @@ class MainActivity : AppCompatActivity(), TaskItemListener
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
+        //implements splash screen for app based on google recommendations this is the best way to implement splash screen.
+        installSplashScreen()
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         viewTaskModel = ViewModelProvider(this).get(ViewTaskModel::class.java)
@@ -33,6 +39,19 @@ class MainActivity : AppCompatActivity(), TaskItemListener
             }
         }
     }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    fun onGroupItemClick(item: MenuItem) {
+
+        // One of the group items (using the onClick attribute) was clicked
+        // The item parameter passed here indicates which item it is
+        // All other menu item clicks are handled by <code><a href="/reference/android/app/Activity.html#onOptionsItemSelected(android.view.MenuItem)">onOptionsItemSelected()</a></code>
+        TODO() //Implement menu functionality for sorting tasks
+    }
+
 
     override fun editTaskItem(taskItem: TaskItemModel)
     {
